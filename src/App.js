3bf46c8b51ee4d32006efa8181f9fc7926b248c0
mohamedsiraj786 <110327550+mohamedsiraj786@users.noreply.github.com>
2023-06-29 00:40:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import clickSound from './click.wav'
+import alhamthulillah from './alhamthulillah.mp3'
+import allahuakbar from './allahuuakbaar.mp3'
+import Asthafirullag from './asthafirullah.mp3';
 
-function App() {
+function Count() {
+
+  const [counts, setCount] = useState(0);
+  const [msg, setMsg] = useState("Asthafirullah");
+
+  function increment() {
+    setCount(counts + 1);
+    
+    if (counts === 33) {
+      setMsg("Alhamthulillah");
+      const audio = new Audio(alhamthulillah);
+    audio.play();
+    }
+
+    if (counts === 66) {
+      setMsg('Allahuakbar');
+      const audio = new Audio(allahuakbar);
+      audio.play();
+    }
+    
+    if (counts === 100) {
+      setCount(0);
+      setMsg("Asthafirullah");
+      const audio = new Audio(Asthafirullag);
+      audio.play();
+    }
+
+    // Creating an audio element
+    const audio = new Audio(clickSound);
+    audio.play();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='layer'>
+      <h1 style={{ textAlign: 'center', color: 'black' }} className='displaylayer'>{counts}</h1>
+      <h1  className='textlayer'>{msg}</h1>
+      <button style={{ display: 'flex', justifyContent: 'center', marginLeft: 'auto', marginRight: 'auto' }} onClick={increment} >Start-Count</button>
     </div>
   );
 }
 
-export default App;
+export default Count;
